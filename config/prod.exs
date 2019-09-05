916b74fc5935a2808d24c4c15fdcee608e21bb08
Seth Webster <sethwebster@gmail.com>
2019-09-05 14:30:17 -0400

    Remove comma

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :airport_lookup_api, AirportLookupApiWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "airport-icao-api", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
   # cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
