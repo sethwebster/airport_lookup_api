aa5e1dc42d44f89bf340228cf2ea7c1522cdee14
Seth Webster <sethwebster@gmail.com>
2019-09-06 13:50:01 -0400

    Added search, name, city, filters
    Lots updated for city/name
    
    City lookup working
    
    Adding filters
    
    added filters

--- lib/airport_lookup_api/airport_data/importer.ex
+++ lib/airport_lookup_api/airport_data/importer.ex
@@ -1,4 +1,9 @@
 defmodule AirportLookupApi.AirportData.Importer do
+  @airports_prefix "airports/"
+  @icao_index "icao/"
+  @name_index "name/"
+  @city_index "city/"
+
   def data do
     path = "./data/airports.json"
     File.read!(path) |>
@@ -8,17 +13,53 @@ defmodule AirportLookupApi.AirportData.Importer do
   def seed_redis_if_necessary do
     if redis_empty?() and !is_seeding?() do
       spawn fn ->
+        IO.puts "Seeding Data..."
         set_seeding true
         send_data_to_redis()
         set_seeding false
+        IO.puts "Data seeding complete..."
       end
     end
   end
 
   defp send_data_to_redis do
-    commands = data()
-    |>Enum.map(fn airport -> put_airport_command(airport) end)
-    Redix.pipeline(:redix, commands)
+    airports = data()
+    
+    icao_commands = airports
+    #|>Enum.take(10000)
+    |>Enum.map(fn airport -> put_airport_commands(airport) end)
+    |>flatten_commands
+    
+    city_commands = airports
+    #|>Enum.take(10000)
+    |>airports_by_facet("municipality")
+    |>Enum.map(fn {key, value} -> set_facet_command("city", key, value) end)
+
+    name_commands = airports
+    #|>Enum.take(10000)
+    |>airports_by_facet("name")
+    |>Enum.map(fn {key, value} -> set_facet_command("name", key, value) end)
+
+    commands = icao_commands ++ city_commands ++ name_commands
+
+    IO.puts "Sending #{length(commands)} to Redix..."
+    {:ok, response} = Redix.pipeline(:redix, commands)
+    IO.puts "Complete."
+  end
+  
+  defp airports_by_facet(data, facet) do
+    Enum.reduce(data, %{}, fn curr, acc -> 
+      { icao, obj } = curr
+      facet = obj[facet]
+      existing = Map.get(acc, facet) || []
+      acc = Map.put(acc, facet, existing ++ [obj])
+    end)
+  end
+
+  defp flatten_commands(commands) do
+    Enum.reduce(commands, [], fn curr, acc ->
+      acc ++ curr 
+    end)
   end
 
   defp is_seeding? do
@@ -29,13 +70,21 @@ defmodule AirportLookupApi.AirportData.Importer do
     Redix.command(:redix, ["SET","system/seeding",in_progress])
   end
 
-  defp put_airport_command(airport) do
+  defp set_facet_command(facet, key, value) do
+    ["SET", "#{@airports_prefix}#{facet}/#{String.upcase(key)}", Poison.encode!(value)]
+  end
+
+  defp put_airport_commands(airport) do
     {icao, data} = airport
-    ["SET", "airports/#{icao}", Poison.encode!(data)]
+    obj = Poison.encode!(data)
+    %{"name" => name, "municipality" => city} = data
+    [
+      ["SET", "#{@airports_prefix}#{@icao_index}#{icao}", obj]
+    ]
   end
 
   defp redis_empty? do
-    {:ok, airport} = Redix.command(:redix, ["GET","airports/ZZZZ"])
+    {:ok, airport} = Redix.command(:redix, ["GET","#{@airports_prefix}#{@icao_index}ZZZZ"])
     if (airport) do
       false
     else
--- lib/airport_lookup_api_web/controllers/airport_controller.ex
+++ lib/airport_lookup_api_web/controllers/airport_controller.ex
@@ -1,10 +1,97 @@
 defmodule AirportLookupApiWeb.AirportController do
   use AirportLookupApiWeb, :controller
 
-  def index(conn, params) do
+  defp filter(data, filters) do
+    filtered = sanitize_filters(filters)
+    |> parse_filters
+    |> apply_filters(data)
+    filtered
+  end
+
+  defp apply_filters(filters, data) do
+    Enum.filter(data, fn item ->
+      filter_results = Enum.map(filters, fn filter ->
+        %{:key => key, :value => value, :func => func} = filter
+        func.(key, value, item)
+      end)
+      Enum.all?(filter_results, fn res -> res end)
+    end)
+  end
+
+  defp sanitize_filters(filters) do
+    raw = String.split(filters, ["[","]",","])
+    |> Enum.filter(fn v -> String.length(v) > 0 end)
+    |> Enum.uniq
+  end
+
+  defp parse_filters(filters) do
+    operators = [
+      "!=": fn k, v, data ->
+        data[k] != v
+      end,
+      "=": fn k, v, data ->
+        data[k] == v
+      end,
+      "->": fn k, v, data ->
+        String.split(v,["|"])
+        |> Enum.find(fn opt -> opt == data[k] end)
+      end
+
+    ]
+    Enum.map(filters, fn item ->
+      operator = Enum.find(operators, fn {k, _} -> String.contains?(item, "#{k}") end)
+      {op, func} = operator
+      [key, value] = String.split(item, "#{op}")
+      %{
+        key: key,
+        value: value,
+        func: func
+      }
+    end)
+  end
+
+  def index(conn, %{"name"=>name, "filters"=> filters }) do
     AirportLookupApi.AirportData.Importer.seed_redis_if_necessary
-    if String.length(params["icao"]) >= 2 do
-      data = AirportLookupApi.AirportData.search(String.upcase(params["icao"]))
+    if (String.length(name) >= 2) do
+      data = AirportLookupApi.AirportData.search(["name"], String.upcase(name))
+      json(conn, %{data: filter(data, filters)})
+    else
+      json(conn, %{error: "Input for `name` requires at least two characters", data: []})
+    end
+  end
+
+  def index(conn, %{ "name" => name } = params) do
+    index(conn, %{"name" => name, "filters" => "[]"})
+  end
+
+  def index(conn, %{"city"=>city, "filters"=>filters}) do
+    AirportLookupApi.AirportData.Importer.seed_redis_if_necessary
+    if (String.length(city) >= 2) do
+      data = AirportLookupApi.AirportData.search(["city"], String.upcase(city))
+      json(conn, %{data: filter(data, filters)})
+    else
+      json(conn, %{error: "Input for `city` requires at least two characters", data: []})
+    end
+  end
+
+  def index(conn, %{ "city" => city } = params) do
+    index(conn, %{"city" => city, "filters" => "[]"})
+  end
+
+  def index(conn, %{"search"=>search}) do
+    AirportLookupApi.AirportData.Importer.seed_redis_if_necessary
+    if (String.length(search) >= 2) do
+      data = AirportLookupApi.AirportData.search(["city","name"], String.upcase(search))
+      json(conn, %{data: data})
+    else
+      json(conn, %{error: "Input for `search` requires at least two characters", data: []})
+    end
+  end
+
+  def index(conn, %{"icao" => icao}) do
+    AirportLookupApi.AirportData.Importer.seed_redis_if_necessary
+    if String.length(icao) >= 2 do
+      data = AirportLookupApi.AirportData.search_icao(String.upcase(icao))
       json(conn, %{data: data})
     else
       json(conn, %{error: "Input for `icao` requires at least two characters", data: []})
